6cf934c6bd5af545b413c4aef64e082b1ebcde41
Cristian Campuzano <cristian_canek@hotmail.com>
2018-07-03 00:24:10 -0500

    Folder and files structure for single linked lists added.

---
+++ 01 Single linked lists/main.c
@@ -0,0 +1,33 @@
+// -----------------------------------------------------------------------------
+// INCLUDES SECTION.
+// -----------------------------------------------------------------------------
+
+#include <"single_linked_list.h">
+#include <"single_linked_list.c">
+
+// -----------------------------------------------------------------------------
+// FUNCTIONS PROTOTYPE SECTION.
+// -----------------------------------------------------------------------------
+
+// -----------------------------------------------------------------------------
+// MAIN PROGRAM.
+// -----------------------------------------------------------------------------
+
+// Main file created for testing the single linked lists functionalities.
+
+int main() {
+
+  // Create a new single linked list.
+  // Verifying if the single linked list is empty or not.
+  // Adding some elements to the single linked list.
+  // Verifying if the single linked list is empty or not.
+  // Updating some elements of the single linked list.
+  // Removing some elements of the single linked list.
+  // Verifying if the single linked list is empty or not.
+
+  return 0;
+}
+
+// -----------------------------------------------------------------------------
+// FUNCTIONS IMPLEMENTATION SECTION.
+// -----------------------------------------------------------------------------
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Data-Structures
 This repository contains my own implementations of the most common basic data structures, such as:
 
-- Linked lists (single).
+- Single linked lists.
